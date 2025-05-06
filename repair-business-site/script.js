0238888f5e9e2c495b6f98a814a5e280c6fb4bff
blomjonathon <126799836+blomjonathon@users.noreply.github.com>
2025-05-06 08:38:07 -0500

    Delete repair-business-site directory

--- repair-business-site/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scheduleForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Your request has been submitted. We will contact you soon!');
-  });
-
-  
-  
